75a4ed03e891bf70c6149a765f876fd77ff3a4bc
luooooob <contact@jiang-yan.com>
2021-12-01 18:53:16 +0800

    Update env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface ImportMetaEnv {
+  VITE_APP_TOKEN_KEY: string
+  VITE_APP_REPORT_DATA_KEY: string
   VITE_APP_OAUTH_URL: string;
   VITE_AUTH_REPORT_DATA_URL: string;
 }
