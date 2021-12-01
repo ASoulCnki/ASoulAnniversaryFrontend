c14f25d14071e9760fa5a342f33d6ef8afd75499
luooooob <contact@jiang-yan.com>
2021-12-01 18:52:31 +0800

    Update windi.config

--- windi.config.js
+++ windi.config.js
@@ -1,41 +1,51 @@
 import { defineConfig } from "windicss/helpers"
 import typography from "windicss/plugin/typography"
-import amimation from "@windicss/plugin-animations"
 
 export default defineConfig({
   extract: {
     include: ["src/**/*.tsx", "index.html"]
   },
-  fontFamily: {
-    sans: [
-      "system-ui",
-      "-apple-system",
-      "Segoe UI",
-      "Roboto",
-      "Helvetica Neue",
-      "Arial",
-      "Noto Sans",
-      "Liberation Sans",
-      "PingFang SC",
-      "Hiragino Sans GB",
-      "Noto Sans CJK SC",
-      "Source Han Sans SC",
-      "Source Han Sans CN",
-      "Microsoft YaHei",
-      "Wenquanyi Micro Hei",
-      "WenQuanYi Zen Hei",
-      "ST Heiti",
-      "SimHei",
-      "WenQuanYi Zen Hei Sharp",
-      "Apple Color Emoji",
-      "Segoe UI Emoji",
-      "Segoe UI Symbol",
-      "Noto Color Emoji",
-      "sans-serif"
-    ]
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "Liberation Sans",
+          "PingFang SC",
+          "Hiragino Sans GB",
+          "Noto Sans CJK SC",
+          "Source Han Sans SC",
+          "Source Han Sans CN",
+          "Microsoft YaHei",
+          "Wenquanyi Micro Hei",
+          "WenQuanYi Zen Hei",
+          "ST Heiti",
+          "SimHei",
+          "WenQuanYi Zen Hei Sharp",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+          "sans-serif"
+        ]
+      },
+      backgroundImage: {
+        default: "url(/background.svg)"
+      }
+    }
   },
+
   plugins: [
-    typography,
-    amimation()
+    typography
   ]
 })
+// backgroundImage: (theme) => {
+//   'hero-lg': "url('/storage/img/sys/lg-hero.jpg')",
+//   'hero-sm': "url('/storage/img/sys/sm-hero.jpg')",
+// },
