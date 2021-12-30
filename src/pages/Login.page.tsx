f40cdb950a88490d88a5cd313928db323b6ef25d
luooooob <contact@jiang-yan.com>
2021-12-31 00:01:27 +0800

    FIx console

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -3,8 +3,5 @@ import { Login } from "~/features/login"
 import type { FC } from "react"
 
 export const LoginPage: FC = ({}) => {
-  console.log("????")
-  return (
-    <Login />
-  )
+  return <Login />
 }
