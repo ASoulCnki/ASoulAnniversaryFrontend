f7b7867b38e60cd6612bdac1983c2e4c61fb8e99
crumbledwall <whj199910@gmail.com>
2022-01-08 17:57:54 +0800

    fix: remove strictmode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,18 @@ import { RedirectPage } from "./Redirect.page"
 // const Report = lazy(() => import("./report").then(module => ({ default: module.Report })))
 
 import type { FC } from "react"
-import { Global } from "~/features/global"
 
 // const defaultFetcher = (url: string, token: string) => fetch(url, { headers: { "X-Au-Token": token } })
 //   .then(res => res.json())
 
 export const App: FC = () => {
   return (
-    <Global>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="Login" element={<LoginPage />} />
-          <Route path="/redirect" element={<RedirectPage/>} />
-        </Routes>
-      </BrowserRouter>
-    </Global>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="Login" element={<LoginPage />} />
+        <Route path="/redirect" element={<RedirectPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
