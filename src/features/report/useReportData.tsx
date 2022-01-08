5962ff5dff57f7b3454096d1caade8f3b26e6b07
crumbledwall <whj199910@gmail.com>
2022-01-08 21:13:29 +0800

    refactor: use swr

--- src/features/report/useReportData.tsx
+++ src/features/report/useReportData.tsx
@@ -1,33 +1,26 @@
 import { useEffect, useState } from "react"
 import { getTokenFromLocalStorage } from "~/helpers/token"
 import type { ReportRequestHeaders, ReportResponse } from "~/interface"
+import useSWR, { SWRResponse } from "swr"
 
 const { VITE_APP_REPORT_DATA_URL } = import.meta.env
 
 export type ReportDataError = "NO_TOKEN" | "INVALID_TOKEN" | "SERVER_ERROR"
 
 export const useReportData = (onError: (err: ReportDataError) => void) => {
-  const [reportData, setReportData] = useState<ReportResponse | null>(null)
-
   const token = getTokenFromLocalStorage()
 
-  useEffect(() => {
-    if (token) {
-      const method = "POST"
-      const headers: ReportRequestHeaders = {
-        "X-Au-Token": token,
-      }
-      window
-        .fetch(VITE_APP_REPORT_DATA_URL, { method, headers })
-        .then<ReportResponse>(res => res.json())
-        .then(res => setReportData(res))
-        .catch(err => {
-          console.warn(err)
-          onError("SERVER_ERROR")
-        })
-    } else {
-      onError("NO_TOKEN")
-    }
-  }, [onError, token])
+  const fetcher = (url: string, token: string) =>
+    fetch(url, {
+      method: "POST",
+      headers: { "X-Au-Token": token },
+    }).then(res => res.json())
+
+  const response: SWRResponse = useSWR<ReportResponse>(
+    [VITE_APP_REPORT_DATA_URL, token],
+    fetcher,
+  )
+  const reportData: ReportResponse = response.data as ReportResponse
+
   return [reportData] as const
 }
