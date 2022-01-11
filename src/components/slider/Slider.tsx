3e5decc6b5b429c8462ac22d75d7afd8b22c81cd
crumbledwall <whj199910@gmail.com>
2022-01-11 16:26:37 +0800

    feat: add animation

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,23 +1,77 @@
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react"
 import type { FC } from "react"
 import { Content } from "./SliderContent"
-// Import Swiper styles
+
 import "swiper/css"
 import type { Data } from "~/interface"
-// import Swiper core and required modules
-import SwiperCore, { Pagination } from "swiper"
 
-// install Swiper modules
-SwiperCore.use([Pagination])
+import SwiperCore from "swiper"
+import { useRef } from "react"
 
 type SliderProps = {
   data: Data
 }
 
+interface slide extends Element {
+  progress: number
+}
+
+interface fillNode extends ChildNode {
+  style: CSSStyleDeclaration
+}
+
 export const Slider: FC<SliderProps> = ({ data }) => {
+  const fills = useRef<HTMLInputElement | null>(null)
+  const effect = {
+    progressMultiplier: 2,
+    prev: {
+      opacity: 0,
+      translate: [0, -128, 0],
+    },
+    next: {
+      opacity: 0,
+      translate: [0, 128, 0],
+    },
+  }
+
+  const setTransition = (swiper: SwiperCore, speed: number) => {
+    fills.current?.childNodes.forEach(ele => {
+      ;(ele as any).style.transitionDuration = `${speed}ms`
+    })
+  }
+
+  const setTranslate = (swiper: SwiperCore) => {
+    const { slides } = swiper
+    fills.current?.childNodes.forEach((ele, index) => {
+      const progress = (slides[index] as slide).progress
+      const rate = 1 - Math.max(Math.min(Math.abs(progress), 1), 0)
+      ;(ele as HTMLElement).style.transform =
+        progress < 0 ? `scale(${rate})` : "scale(1)"
+    })
+  }
+
   return (
-    <>
-      <Swiper direction={"vertical"} autoHeight={true} className="mySwiper">
+    <div className="relative">
+      <div
+        className="fills absolute w-[400vw] h-[400vh] ml-[-150vw] mt-[-50vh]"
+        ref={fills}
+      >
+        <div className="fill absolute bg-slate-500 w-full h-full rounded-full"></div>
+        <div className="fill absolute bg-lime-200 w-full h-full rounded-full scale-0"></div>
+        <div className="fill absolute bg-sky-500 w-full h-full rounded-full scale-0"></div>
+        <div className="fill absolute bg-teal-500 w-full h-full rounded-full scale-0"></div>
+        <div className="fill absolute bg-fuchsia-400 w-full h-full rounded-full scale-0"></div>
+      </div>
+      <Swiper
+        onSetTranslate={setTranslate}
+        onSetTransition={setTransition}
+        direction={"vertical"}
+        autoHeight={true}
+        className="mySwiper"
+        watchSlidesProgress={true}
+        creativeEffect={effect}
+        speed={800}
+      >
         <SwiperSlide>
           <Content>{data.reply_first.content}</Content>
         </SwiperSlide>
@@ -34,6 +88,6 @@ export const Slider: FC<SliderProps> = ({ data }) => {
           <Content>Slide 5</Content>
         </SwiperSlide>
       </Swiper>
-    </>
+    </div>
   )
 }
--- src/components/slider/SliderContent.tsx
+++ src/components/slider/SliderContent.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react"
 
 export const Content: FC = ({ children }) => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="text-3xl text-white h-screen w-screen flex justify-center items-center">
       {children}
     </div>
   )
