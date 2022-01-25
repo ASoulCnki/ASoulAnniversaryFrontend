ac2341224033c20ae854361893adf85a66974cb0
crumbledwall <whj199910@gmail.com>
2022-01-25 15:56:47 +0800

    chore: add arrow pic and change backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const typography = require("@tailwindcss/typography")
-const clamp = require('@tailwindcss/line-clamp')
+const clamp = require("@tailwindcss/line-clamp")
 
 module.exports = {
   content: [
@@ -51,27 +51,41 @@ module.exports = {
         ]
       },
       backgroundImage: {
-        default: "url(/backgrounds/background.svg)",
+        default: "url(/backgrounds/background.png)",
         default1: "url(/backgrounds/background1.svg)",
-        default2: "url(/backgrounds/background2.svg)",
-        default3: "url(/backgrounds/background3.svg)",
-        default4: "url(/backgrounds/background4.svg)",
+        default2: "url(/backgrounds/background2.png)",
+        default3: "url(/backgrounds/background3.png)",
+        default4: "url(/backgrounds/background4.png)",
         default5: "url(/backgrounds/background5.svg)",
-        default6: "url(/backgrounds/background6.svg)",
+        default6: "url(/backgrounds/background6.png)",
         default7: "url(/backgrounds/background7.svg)",
-        default8: "url(/backgrounds/background8.svg)",
-        default9: "url(/backgrounds/background9.svg)",
-        default10: "url(/backgrounds/background10.svg)",
-        default11: "url(/backgrounds/background11.svg)",
-        default12: "url(/backgrounds/background12.svg)",
+        default8: "url(/backgrounds/background8.png)",
+        default9: "url(/backgrounds/background9.png)",
+        default10: "url(/backgrounds/background10.png)",
+        default11: "url(/backgrounds/background11.png)",
+        default12: "url(/backgrounds/background12.png)",
+        arrow: "url(/arrow.svg)",
         "apple-text": "linear-gradient(75deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 33.33%,  rgba(255,255,255,0) 66.67%, rgba(255,255,255,0) 100%)"
       },
       backgroundSize: {
         "size-apple-text": "300% 100%"
       },
-      gridTemplateRows: {
-        'two': 'repeat(auto-fill, 48%)',
-        'three': 'repeat(auto-fill, 30%)'
+
+      animation: {
+        arrow: "arrow 2s ease-in-out infinite;"
+      },
+      keyframes: {
+        arrow: {
+          "0%": {
+            transform: "translateY(40%)"
+          },
+          "50%": {
+            transform: "translateY(0%)"
+          },
+          "to": {
+            transform: "translateY(40%)"
+          }
+        }
       }
     }
   },
